5d2da22833686ab8b97cab2325de0224bdc46b87
Cramney <87803204+Cramney@users.noreply.github.com>
2021-08-24 11:15:44 +0800

    Initial commit

---
+++ 1.js
@@ -0,0 +1,18 @@
+var para1 = document.getElementById("searchbox")
+var para2 = document.getElementById("left")
+var para3 = document.getElementById("search-box")
+function mOver(obj){
+	obj.style.color="black";
+}
+function mOut(obj){
+	obj.style.color="#545c63";
+}
+function tap(obj){
+	para1.style.opacity="100";
+}
+function show(){
+	para3.style.display="block";
+}
+function disshow(){
+	para3.style.display="none";
+}
